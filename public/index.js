82557eac815d9b7a3880f34ceefe0e8133d92c7b
Robp773 <robpeterman573@gmail.com>
2018-10-27 23:00:25 -0400

    added autoscrolling on nav bar clicks and "fade-in"  animation for contact-schedule section on user scroll

---
+++ public/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+$(document).ready(function () {
+  $(window).on('scroll', function (e) {
+    console.log(e.currentTarget.scrollY)
+    if (e.currentTarget.scrollY >= 1200) {
+      $('.contact-schedule').animate({
+        opacity: 1
+      }, 1500)
+    }
+  })
+
+  $('#instructions-intro, #gallery, #schedule, #contact, #intro').click(function (e) {
+   
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(`.${e.currentTarget.attributes.id.value}`).offset().top
+    }, 750)
+  })
+})
