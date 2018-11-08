cdcfd52312d4046c518e93910c68196ffa6b15cd
Robp773 <robpeterman573@gmail.com>
2018-11-07 21:05:51 -0500

    added font awesome cdn, step one video content, refresh button for loading page, and basic visual styling

--- public/pages/instructions.js
+++ public/pages/instructions.js
@@ -4,36 +4,66 @@ let steps = ['one', 'two', 'three', 'four', 'five']
 // counter for where user is in steps
 let stepsIndex = 0
 
+let loadingCounter = 0;
+
 $(function () {
+
+  let loadingTracker = setInterval(function () {
+    console.log(loadingCounter)
+    if (loadingCounter === 59) {
+      $('.loading').animate({
+        opacity: 0
+      }, 500, function () {
+        $('.loading').css('display', 'none')
+        $('.steps-intro').css('display', 'flex')
+        $('.step-parent').css('display', 'flex')
+        setTimeout(function () {
+          $('.steps-intro').hide()
+          // display first step
+          $(`.step-${steps[stepsIndex]}`).css('opacity', '0')
+          $(`.step-${steps[stepsIndex]}`).css('display', 'flex')
+          $(`.step-${steps[stepsIndex]}`).animate({
+            opacity: 1
+          }, 500)
+          // 67000
+        }, 5000)
+      })
+      clearInterval(loadingTracker)
+    }
+    loadingCounter++;
+  }, 1000)
   // set play back rate of bg video to be 50% of normal time
   // cuts down on video resets while 60 seconds of loading is passing
   // not working with jQuery for unknown reason
   var vid = document.getElementById("videoEl");
-  vid.playbackRate = 0.5;
+  vid.playbackRate = 0.75;
   // next button clicks
+
+  $('.loading__refresh-btn').on('click', function () {
+    loadingCounter = 0;
+  })
   $('.next').click(function () {
     //   if user is at last step, return
     if (stepsIndex === steps.length - 1) {
       return
     }
-
     // current visible step
     let element = $(`.step-${steps[stepsIndex]}`)
-    // fade current step to 0 opacity
     element.animate({
       opacity: 0
-    }, 1000, function () {
-      // when opacity is at 0 display: none
+    }, 500, function () {
+      console.log('done') 
       element.css('display', 'none')
       stepsIndex++
-      //   make next element visible - elements by default are display: none
-      // with 0 opacity
+      //  make next element visible - elements by default are display: none
+      $(`.step-${steps[stepsIndex]}`).css('opacity', '0')
       $(`.step-${steps[stepsIndex]}`).css('display', 'flex')
-      // raise opacity from 0 to 1
       $(`.step-${steps[stepsIndex]}`).animate({
         opacity: 1
-      }, 1000)
+      }, 500)
+
     })
+
   })
 
   $('.prev').click(function () {
@@ -41,38 +71,60 @@ $(function () {
       return;
     }
     let element = $(`.step-${steps[stepsIndex]}`)
+
+
     element.animate({
       opacity: 0
-    }, 1000, function () {
+    }, 500, function () {
+      console.log('done') 
       element.css('display', 'none')
       stepsIndex--
+      //  make next element visible - elements by default are display: none
+      $(`.step-${steps[stepsIndex]}`).css('opacity', '0')
       $(`.step-${steps[stepsIndex]}`).css('display', 'flex')
       $(`.step-${steps[stepsIndex]}`).animate({
         opacity: 1
-      }, 1000)
+      }, 500)
+
     })
   })
-  runLoading()
+
+  $('.step-one__vid-1').on('click', function () {
+    $('.step-one__video').attr('src', '../img/fast.mp4').css('display', 'block');
+    $('.step-one__icon-parent').css('display', 'none');
+  })
+
+  $('.step-one__vid-2').on('click', function () {
+    $('.step-one__video').attr('src', '../img/slow.mp4').css('display', 'block');
+    $('.step-one__icon-parent').css('display', 'none');
+  })
+
+  $('.home-btn').on('click', function () {
+    console.log('test worked')
+  })
+  // runLoading()
 })
 
 // runs on page load (after begin button is pressed)
 // runs loading screen for x seconds
 // fades loading screen opacity out, sets it to display none, and makes intro and step elements visible
-function runLoading() {
-  setTimeout(function () {
-    $('.loading').animate({
-      opacity: 0
-    }, 1000, function () {
-      $('.loading').css('display', 'none')
-      $('.steps-intro').css('display', 'flex')
-      $('.step-parent').css('display', 'flex')
-    })
-  }, 60000)
-  // once intro is finished, it hides it and brings up the first step
-  // first timeout amount + 7000 ms to account for all animation durations - keeps transitions smooth
-  setTimeout(function () {
-    $('.steps-intro').hide()
-    // display first step
-    $('.step-one').css('display', 'flex')
-  }, 67000)
-}
+// function runLoading() {
+//   setTimeout(function () {
+//     $('.loading').animate({
+//       opacity: 0
+//     }, 1000, function () {
+//       $('.loading').css('display', 'none')
+//       $('.steps-intro').css('display', 'flex')
+//       $('.step-parent').css('display', 'flex')
+//     })
+//   }, 60000)
+//   // 60000
+//   // once intro is finished, it hides it and brings up the first step
+//   // first timeout amount + 7000 ms to account for all animation durations - keeps transitions smooth
+//   setTimeout(function () {
+//     $('.steps-intro').hide()
+//     // display first step
+//     $('.step-one').css('display', 'flex')
+//     // 67000
+//   }, 67000)
+// }
